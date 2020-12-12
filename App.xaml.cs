bc70e5c6fce88a4eff53bb83d1257433b7594574
adamkekesi <kekesiadi@gmail.com>
2020-12-12 17:15:42 +0100

    Átnevezések

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Tesla_autókonfigurátor
+namespace TeslaCarConfigurator
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TeslaCarConfigurator
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Windows_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Windows.Title = $"{Windows.ActualWidth} x {Windows.ActualHeight}";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CloseWindow closeWindow = new CloseWindow();
+            closeWindow.Show();
+        }
+    }
+}
