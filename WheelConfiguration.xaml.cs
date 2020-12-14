235657cda41892a298ba849a9cc9b8529aabc10e
Sziszi <Sziszi@DESKTOP-SRKA5FH>
2020-12-14 16:46:40 +0100

    WheelsOldalLétrehozása

---
+++ WheelConfiguration.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeslaCarConfigurator
+{
+    /// <summary>
+    /// Interaction logic for WheelConfiguration.xaml
+    /// </summary>
+    public partial class WheelConfiguration : Page
+    {
+        public WheelConfiguration()
+        {
+            InitializeComponent();
+        }
+
+        private void Windows_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Windows.Title = $"{Windows.ActualWidth} x {Windows.ActualHeight}";
+
+            if (Windows.ActualWidth <= 710)
+            {
+                Menu.Width = 230;
+            }
+            else
+            {
+                Menu.Width = 400;
+            }
+        }
+    }
+}
