9f49b8ded72384ba67b8d177530f03493a73aae7
adamkekesi <kekesiadi@gmail.com>
2020-12-14 17:52:57 +0100

    akkumulátor

---
+++ BatteryConfiguration.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TeslaCarConfigurator.Helpers;
+
+namespace TeslaCarConfigurator
+{
+    public partial class BatteryConfiguration : PageBase
+    {
+        public BatteryConfiguration()
+        {
+            InitializeComponent();
+        }
+
+        private void Windows_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Windows.Title = $"{Windows.ActualWidth} x {Windows.ActualHeight}";
+
+            if (Windows.ActualWidth <= 710)
+            {
+                Menu.Width = 230;
+            }
+            else
+            {
+                Menu.Width = 400;
+            }
+        }
+    }
+}
