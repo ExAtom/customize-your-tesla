53262ba57884493e86d2f3be121434e4af916065
Sziszi <Sziszi@DESKTOP-SRKA5FH>
2020-12-14 23:24:54 +0100

    Kerekek kiválasztása

--- WheelConfiguration.xaml.cs
+++ WheelConfiguration.xaml.cs
@@ -43,9 +43,29 @@ namespace TeslaCarConfigurator
             //Config.Wheels
         }
 
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        private void rbWheelSet_Click(object sender, RoutedEventArgs e)
         {
-
+            RadioButton currentButton = (RadioButton)sender;
+            if (currentButton.Name == "rbWheel1")
+            {
+                tbInfos.Text = "Az Aero FX530-as típusú kereket legfőképpen városi közlekedésre ajánljuk. Kiváló tapadást bitosít az aszfalhot, így a hirtelen fékezések sem okozhatnak problémát.";
+                tbPrice.Text = "Ára: 25.000 Ft";
+            }
+            if (currentButton.Name == "rbWheel2")
+            {
+                tbInfos.Text = "A Sport SP002-őt nevéhez híven olyanoknak ajánjuk, akik szeretik a sebességhatárt túllépni, akár versenyzés közben. A 20\"-as kerékátmérő biztosítja a 450 km/h-ás sebesség elérését.";
+                tbPrice.Text = "Ára: 82.000 Ft";
+            }
+            if (currentButton.Name == "rbWheel3")
+            {
+                tbInfos.Text = "A Turbine T54-es tipikusan az a kerék, melyet terepviszonyokra terveztek. Az 50 cm-es kerékvastagság és a speciális rovátkázsának köszönhetően homokban, hóban, mocsárban sincs akadály.";
+                tbPrice.Text = "Ára: 56.000 Ft";
+            }
+            if (currentButton.Name == "rbWheel4")
+            {
+                tbInfos.Text = "A Silver SU478-as típus különlegessége speciális kialakítása révén, hogy téli és nyári abroncsként egyaránt funkciónál, így nem kell azt cserélgetni. Leginkább városi és országúti terepre ajánljuk.";
+                tbPrice.Text = "Ára: 69.000 Ft";
+            }
         }
     }
 
