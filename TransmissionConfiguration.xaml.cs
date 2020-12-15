f216e6de0b50ce4bee9d321635de6c6a1e6f6809
adamkekesi <kekesiadi@gmail.com>
2020-12-15 17:48:02 +0100

    kerekek oldal elmentődik

---
+++ TransmissionConfiguration.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TeslaCarConfigurator.Helpers;
+
+namespace TeslaCarConfigurator
+{
+    public partial class TransmissionConfiguration : PageBase
+    {
+        private List<string[]> chosenTransmissionTexts;
+
+
+        public TransmissionConfiguration()
+        {
+            chosenTransmissionTexts = new List<string[]>()
+            {
+                new string[2]{ "", "Alapból jár hozzá" },
+                new string[2]{ "A 70 kWh-s akkumulátor már egy kicsivel erősebb, ideális választás városok között ingázóknak.", "Ára: 30.000FT" },
+            };
+            InitializeComponent();
+
+
+        }
+
+        public override void OnAttachedToFrame()
+        {
+            byte chosenBatteryIndex = Config?.Battery?.CapacityIndex ?? 0;
+            Viewbox selectedVb = (Viewbox)batteryOptionsContainer.Children[chosenBatteryIndex];
+            RadioButton selected = (RadioButton)selectedVb.Child;
+            selected.IsChecked = true;
+        }
+
+        private void Windows_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Windows.Title = $"{Windows.ActualWidth} x {Windows.ActualHeight}";
+
+            if (Windows.ActualWidth <= 710)
+            {
+                Menu.Width = 230;
+            }
+            else
+            {
+                Menu.Width = 400;
+            }
+        }
+
+        private void rbBatteryType_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = (RadioButton)sender;
+            string name = rb.Name;
+            byte index = byte.Parse(name.Replace("rbBatteryType", ""));
+            string[] texts = chosenTransmissionTexts[index];
+            tbInfos.Text = texts[0];
+            tbPrice.Text = texts[1];
+            if (Config == null)
+            {
+                return;
+            }
+            Config.Battery.CapacityIndex = index;
+        }
+    }
+}
