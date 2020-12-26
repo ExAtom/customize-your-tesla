8695bb14bf601ba96ab90dc7988f6769fdd8e9d8
adamkekesi <kekesiadi@gmail.com>
2020-12-26 17:26:06 +0100

    CountryInfo-országinfókat tároló class CountryService-országinfók letöltése

---
+++ Services/CountryInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeslaCarConfigurator.Services
+{
+    public class CountryInfo
+    {
+        public string[] CallingCodes { get; set; }
+
+        public string Flag { get; set; }
+
+        public string NativeName { get; set; }
+    }
+}
---
+++ Services/CountryService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace TeslaCarConfigurator.Services
+{
+    public static class CountryService
+    {
+        private const string apiUrl = "https://restcountries.eu/rest/v2/all?fields=callingCodes;flag;nativeName";
+
+        private static HttpClient httpClient = new HttpClient();
+
+        public static async Task<CountryInfo[]> FetchCountryInfos()
+        {
+            var response = await httpClient.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+            string content = await response.Content.ReadAsStringAsync();
+            CountryInfo[] countryInfos = JsonSerializer.Deserialize<CountryInfo[]>(content, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            return countryInfos;
+        }
+    }
+}
