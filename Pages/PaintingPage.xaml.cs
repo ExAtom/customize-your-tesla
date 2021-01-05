42e366f0d7b78b2635674a8d55de63b9f63b0042
adamkekesi <kekesiadi@gmail.com>
2021-01-05 22:53:24 +0100

    Lapok áthelyezve

--- Pages/PaintingPage.xaml.cs
+++ Pages/PaintingPage.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TeslaCarConfigurator.Helpers;
 
-namespace TeslaCarConfigurator
+namespace TeslaCarConfigurator.Pages
 {
     /// <summary>
     /// Interaction logic for PaintingPage.xaml
