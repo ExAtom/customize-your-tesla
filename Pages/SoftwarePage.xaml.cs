d161a50ebc13d751625fa47e60171d0614871a94
ExAtom <menyhart.atika@gmail.com>
2021-01-08 15:57:57 +0100

    SoftwarePage

---
+++ Pages/SoftwarePage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TeslaCarConfigurator.Helpers;
+
+namespace TeslaCarConfigurator.Pages
+{
+    /// <summary>
+    /// Interaction logic for InteriorPage.xaml
+    /// </summary>
+    public partial class SoftwarePage : PageBase
+    {
+        public SoftwarePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Windows_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            if (Windows.ActualWidth <= 710)
+            {
+                Menu.Width = 230;
+            }
+            else
+            {
+                Menu.Width = 400;
+            }
+
+        }
+
+        public override void OnAttachedToFrame()
+        {
+            //byte chosenTypeIndex = Config?.Interior?.MaterialIndex ?? 0;
+            //RadioButton selected = null;
+
+            //if (chosenTypeIndex == 0)
+            //{
+            //    selected = iMaterial1;
+            //}
+
+            //if (chosenTypeIndex == 1)
+            //{
+            //    selected = iMaterial2;
+            //}
+
+            //if (chosenTypeIndex == 2)
+            //{
+            //    selected = iMaterial3; 
+            //}
+
+            //if (chosenTypeIndex == 3)
+            //{
+            //    selected = iMaterial4;
+            //}
+
+            //if (selected == null)
+            //{
+            //    return;
+            //}
+            //selected.IsChecked = true;
+        }
+
+        private void softwareSet_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox currentButton = (CheckBox)sender;
+            if (currentButton.Name == "selfdriving")
+            {
+                tbInfos.Text = "A bio műanyag egy erősebb és környezetkímélőbb alternatíva. A textúrájának kidolgozása az aszfaltra hasonlít, ezzel egyé válhat az úttal, amin éppen halad.";
+                tbPrice.Text = "Alap anyag";
+            }
+            if (currentButton.Name == "gps")
+            {
+                tbInfos.Text = "Az öntött fa nem igazi fa, külseje üveges, de ez adja át a legtermészetesebb hatást. Mintha csak visszautaztunk volna az időben, de az autó megtartja prémium érzetét.";
+                tbPrice.Text = "Ára: 560.000 Ft";
+            }
+            if (currentButton.Name == "headlightAssistant")
+            {
+                tbInfos.Text = "A jegelt üveg egy speciális felületcsiszolással készül, ezzel erősítve magát az anyagot, valamint az ujjlenyomatoktól is védi. A végeredmény egy hó textúrájára hasonlít. A legjobb választás téli kocsikázásra.";
+                tbPrice.Text = "Ára: 665.000 Ft";
+            }
+            if (currentButton.Name == "adaptiveLights")
+            {
+                tbInfos.Text = "A krokodilbőr a legritkább fajtájú, Gangeszi aprókrokodil bőre. Elképesztően jóminőségű, tapintása eszméletlen és a szaga új autójéra hasonlít.";
+                tbPrice.Text = "Ára: 1.029.000 Ft";
+            }
+
+            if (Config == null)
+            {
+                return;
+            }
+
+            //byte index = (byte)(byte.Parse(currentButton.Name.Replace("iMaterial", "")) - 1);
+            //Config.Interior.MaterialIndex = index;
+        }
+    }
+}
