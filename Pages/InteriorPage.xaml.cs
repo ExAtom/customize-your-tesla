93d195756f9b50707dcced3ecf174c10b280aa04
ExAtom <menyhart.atika@gmail.com>
2021-01-10 18:44:08 +0100

    Leírások

--- Pages/InteriorPage.xaml.cs
+++ Pages/InteriorPage.xaml.cs
@@ -87,6 +87,7 @@ namespace TeslaCarConfigurator.Pages
         private void iRadioSet_Checked(object sender, RoutedEventArgs e)
         {
             RadioButton currentButton = (RadioButton)sender;
+
             if (currentButton.Name == "iMaterial1")
             {
                 tbInfos.Text = "A bio műanyag egy erősebb és környezetkímélőbb alternatíva. A textúrájának kidolgozása az aszfaltra hasonlít, ezzel egyé válhat az úttal, amin éppen halad.";
@@ -106,6 +107,22 @@ namespace TeslaCarConfigurator.Pages
             {
                 tbInfos.Text = "A krokodilbőr a legritkább fajtájú, Gangeszi aprókrokodil bőre. Elképesztően jóminőségű, tapintása eszméletlen és a szaga új autójéra hasonlít.";
                 tbPrice.Text = "Ára: 1.029.000 Ft";
+            }
+
+            if (currentButton.Name == "iColor1")
+            {
+                tbInfos.Text = "Fekete. Milyen egyszerű. Milyen elegáns. Egyszerűen tökéletes. Az űrre és annak végtelenül csodálatosságára emlékeztet. Csodás választás.";
+                tbPrice.Text = "Alap szín";
+            }
+            if (currentButton.Name == "iColor2")
+            {
+                tbInfos.Text = "A fehér kitisztultság, felsőbbrendűségre sugall. Vezetés közben páratlanul kiemelkedő érzése lehet a vezetőnek.";
+                tbPrice.Text = "Ára: 500.000 Ft";
+            }
+            if (currentButton.Name == "iColor3")
+            {
+                tbInfos.Text = "A barna a legtermészetesebb színünk. Az öntött fával vagy krokodilbőrrel együtt teljes az összhatás. Klasszikus és csodás.";
+                tbPrice.Text = "Ára: 600.000 Ft";
             }
 
             if (Config == null)
@@ -127,25 +144,30 @@ namespace TeslaCarConfigurator.Pages
         private void iCheckboxSet_Checked(object sender, RoutedEventArgs e)
         {
             CheckBox currentButton = (CheckBox)sender;
-            if (currentButton.Name == "iMaterial1")
+            if (currentButton.Name == "seatHeating")
             {
-                tbInfos.Text = "A bio műanyag egy erősebb és környezetkímélőbb alternatíva. A textúrájának kidolgozása az aszfaltra hasonlít, ezzel egyé válhat az úttal, amin éppen halad.";
-                tbPrice.Text = "Alap anyag";
+                tbInfos.Text = "Ülés fűtés és hűtés. Bármilyen idő legyen kint, a legkényelmesebb vezetést nyújtja.";
+                tbPrice.Text = "Ára: 150.000";
             }
-            if (currentButton.Name == "iMaterial2")
+            if (currentButton.Name == "stearingWheelHeating")
             {
-                tbInfos.Text = "Az öntött fa nem igazi fa, külseje üveges, de ez adja át a legtermészetesebb hatást. Mintha csak visszautaztunk volna az időben, de az autó megtartja prémium érzetét.";
-                tbPrice.Text = "Ára: 560.000 Ft";
+                tbInfos.Text = "Kormánykerék fűtése és hűtése. Reggeli induláskor gyakran kényelmetlenül forró vagy hideg lehet a kormány, így az nehezíti a kormányzást. Ennek megoldása ez a beállítás.";
+                tbPrice.Text = "Ára: 110.000 Ft";
             }
-            if (currentButton.Name == "iMaterial3")
+            if (currentButton.Name == "spineSupport")
             {
-                tbInfos.Text = "A jegelt üveg egy speciális felületcsiszolással készül, ezzel erősítve magát az anyagot, valamint az ujjlenyomatoktól is védi. A végeredmény egy hó textúrájára hasonlít. A legjobb választás téli kocsikázásra.";
-                tbPrice.Text = "Ára: 665.000 Ft";
+                tbInfos.Text = "Hosszú utakon nagyban segíthet a gerinctámasz beszerelése. Britt tudósok szerint még a látása is javul tőle.";
+                tbPrice.Text = "Ára: 69.420 Ft";
             }
-            if (currentButton.Name == "iMaterial4")
+            if (currentButton.Name == "sunlightProtection")
             {
-                tbInfos.Text = "A krokodilbőr a legritkább fajtájú, Gangeszi aprókrokodil bőre. Elképesztően jóminőségű, tapintása eszméletlen és a szaga új autójéra hasonlít.";
-                tbPrice.Text = "Ára: 1.029.000 Ft";
+                tbInfos.Text = "Elég lesz a borzalmas műanyag fényvisszaverő lapokból, amiket senki se bír normálisan az ablkba. Ez az új technológia az autó kikapcsolásakor elsőtétíti az ablakokat, amin kereszül a napfény nem jut be az autóba.";
+                tbPrice.Text = "Ára: 650.000 Ft";
+            }
+            if (currentButton.Name == "darkenedWindows")
+            {
+                tbInfos.Text = "Kívülről senki se lát be, de belülről ki lát mindenki. Az eleganciapontja is kiemelkedően növekszik a járműnek ennek használatakor.";
+                tbPrice.Text = "Ára: 190.000 Ft";
             }
 
             if (Config == null)
