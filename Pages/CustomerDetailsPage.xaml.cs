f679e7e65ec771132ac1bbb4ca4d1ea8ed765782
ExAtom <menyhart.atika@gmail.com>
2021-01-12 16:34:21 +0100

    Számla1

--- Pages/CustomerDetailsPage.xaml.cs
+++ Pages/CustomerDetailsPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -42,6 +43,26 @@ namespace TeslaCarConfigurator.Pages
         private void BuyCar(object sender, RoutedEventArgs e)
         {
             MessageBarController.ShowSuccess("Sikeres vásárlás!", 9000);
+
+            var receiptOut = new List<string>();
+
+            receiptOut.Add("Tesla Autó Konfigurátor\nSzámla\n\nKöszönjük, hogy nálunk vásárolt!\n----------\n");
+            receiptOut.Add($"Model: {Config.CarModel.ModelName}\n\t+ {String.Format("{0:n0}", Config.CarModel.CalculateAdditionalPrices())} Ft");
+            receiptOut.Add($"Akkumulátor: {Config.Battery.Capacity} kWh\n\t+ {String.Format("{0:n0}", Config.Battery.CalculateAdditionalPrices())} Ft");
+            receiptOut.Add($"Festés: {Config.Painting.Color}\n\t+ {Config.Painting.CalculateAdditionalPrices()} Ft");
+            receiptOut.Add($"Kerekek: {Config.Wheels.Type}\n\t+ {Config.Wheels.CalculateAdditionalPrices()} Ft");
+            receiptOut.Add($"Váltó: {Config.Transmission.Type}\n\t+ {Config.Transmission.CalculateAdditionalPrices()}");
+            receiptOut.Add($"Belső anyag: {Config.Interior.Material}\n\t+ {Config.Interior.MaterialPrices}");
+            receiptOut.Add($"");
+            receiptOut.Add($"");
+            receiptOut.Add($"");
+            receiptOut.Add($"");
+            receiptOut.Add($"");
+            receiptOut.Add($"");
+            receiptOut.Add($"Teljes ár: {String.Format("{0:n0}", Config.TotalPrice)} Ft");
+            receiptOut.Add($"");
+
+            File.WriteAllLines("számla.txt", receiptOut);
         }
 
         private void OnLoadingFailed()
