8a100e8d70ad8e5daba0c7089406f83aca3d0e08
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2021-01-13 01:18:56 +0100

    Mentés kártya design

---
+++ UserControls/SavedConfigCard.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeslaCarConfigurator.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SavedConfigCar.xaml
+    /// </summary>
+    public partial class SavedConfigCard : UserControl
+    {
+        public SavedConfigCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
